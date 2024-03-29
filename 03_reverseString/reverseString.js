949e4850a056e835199a64560371829c37f004ac
nagar@raspizero2 <nagaratokuma@gmail.com>
2024-03-30 00:51:06 +0900

    Finished up to number 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(original) {
+    let reversedString = "";
+    for(let i = 0; i < original.length; i++) {
+        reversedString += original.charAt(original.length - 1 - i)
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
